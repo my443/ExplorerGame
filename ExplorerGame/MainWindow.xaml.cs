7ddcb9669ce7d3ccbc7f7eab0b09e3f5f0d94d26
John van Dijk <fortyfour-three@teksavvy.com>
2023-12-14 06:32:56 -0500

    Refactor. Removed commented code.

--- ExplorerGame/MainWindow.xaml.cs
+++ ExplorerGame/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace ExplorerGame
 
         public MainWindow()
         {
- 
+
             this.InitializeComponent();
             _gameLoop = new GameLoop();
             _gameLoop.Load(Game);
@@ -42,7 +42,6 @@ namespace ExplorerGame
             CompositionTarget.Rendering += GameLoopUpdate;                  // This subscribes to the CompositionTarget.Rendering event.
                                                                             // Called once per frame refresh (usually 60fps)
                                                                             // https://learn.microsoft.com/en-us/dotnet/api/system.windows.media.compositiontarget.rendering?view=windowsdesktop-8.0
-
         }
 
         private void GameLoopUpdate(object sender, object e)
@@ -50,63 +49,10 @@ namespace ExplorerGame
             Canvas.SetLeft(SoccerBall, Game.ballLeft);
         }
 
-        //public void MainWindow_Load()
-        //{
-        //    _gameLoop = new GameLoop();
-        //    MainWindow mainWindow = this;
-        //    _gameLoop.MainWindow = mainWindow;
-
-        //    _gameLoop.Render();
-        //    Canvas.SetLeft(SoccerBall, 150);
-
-        //}
-
-        //public void moveBall()
-        //{
-        //    int nextMovePosition = canvasLeftPosition + (direction * 20);
-
-        //    if (nextMovePosition > 400) {
-        //        direction = -1;
-        //        nextMovePosition = 400;
-        //    }
-        //    else if (nextMovePosition < 20)
-        //    {
-        //        direction = 1;
-        //        nextMovePosition = 20;
-        //    }
-
-        //    canvasLeftPosition = nextMovePosition;
-
-        //    Canvas.SetLeft(SoccerBall, nextMovePosition);
-        //}
-
-        //public void moveBall()
-        //{
-        //    int nextMovePosition = canvasLeftPosition + (direction * 20);
-
-        //    if (nextMovePosition > 400)
-        //    {
-        //        direction = -1;
-        //        nextMovePosition = 400;
-        //    }
-        //    else if (nextMovePosition < 20)
-        //    {
-        //        direction = 1;
-        //        nextMovePosition = 20;
-        //    }
-
-        //    canvasLeftPosition = nextMovePosition;
-
-        //    Canvas.SetLeft(SoccerBall, nextMovePosition);
-        //}
-
-
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked. Ball at:"+ Game.ballLeft.ToString();
-            //Game.moveBall();
+            myButton.Content = "Clicked. Ball at:" + Game.ballLeft.ToString();
             Canvas.SetLeft(SoccerBall, Game.ballLeft);
-            //moveBall();
         }
     }
 }
